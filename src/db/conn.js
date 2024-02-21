55cc94da7e82be70cb9f14394fe46f5a9851e254
S-Siri <123954769+S-Siri@users.noreply.github.com>
2024-02-21 18:34:43 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/crimsonRegistration").then(()=>{
+mongoose.connect("mongodb+srv://ssiri2409:2ORBp3EQSkZaKXha@crimson.ognoqej.mongodb.net/?retryWrites=true&w=majority").then(()=>{
     console.log(`connection successful`);
 }).catch((e)=>{
     console.log(e);
